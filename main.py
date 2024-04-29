cad9c655bf5aac7bb2168bd27c618240b3dca8a2
Phan MH <phan.manh0hung@gmail.com>
2024-04-29 13:44:08 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from random import randint
+
+digits = 5
+health = 10
+secret = randint(0, 10**(digits) - 1)
+
+while True:
+    guess = int(input("Guess the number: "))
+    correct = 0
+    
+    if secret == guess:
+        print("Correct!")
+        break
+    
+    # iteration and comparison
+    for i in range(1, digits+1):
+        if (secret//10**i) % 10 == (guess//10**i) % 10:
+            correct += 1
+    
+    print(correct)
+    print(secret)
