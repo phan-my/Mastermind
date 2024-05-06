4e9aff1467e0239c7665434a43fa970e9f3294d4
Phan MH <phan.manh0hung@gmail.com>
2024-05-06 12:19:22 +0200

    Fix bug of correctPosition

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ guess = -1
 
 while secret != guess: # NICHT while True
     guess = int(input("Guess the number: "))
-    correct = 0
+    correctPosition = 0
     
     # iteration and comparison
-    for i in range(1, digits):
-        if (secret//10**i) % 10 == (guess//10**i) % 10:
-            correct += 1
+    # BUG for secret = 189, we have 999 that does not register
+    for i in range(0, digits):
+        if (secret//(10**i)) % 10 == (guess//(10**i)) % 10:
+            correctPosition += 1
     
-    print(correct)
+    # Problem: how to remember w duplicate numbers
+    # (secret = 123; guess = 242; correctNumber == 1)
+    
+    print(correctPosition)
     # print(secret)
     
 print("Correct!")
