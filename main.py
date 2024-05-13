577d8bdffcf6b5ee1689d1e0ec31b7494133eb13
Phan MH <phan.manh0hung@gmail.com>
2024-05-13 18:48:47 +0200

    Split iterations into two; convoluted but bug fixed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 def nth_digit(x, nthDigit):
-    return x//10**nthDigit % 10
+    return x//(10**nthDigit) % 10
 
 digits = 3
 health = 10
@@ -20,25 +20,28 @@ while secret != guess: # NICHT while True
     for i in range(digits):
         SecretArray[i] = nth_digit(secret, i)
     
+    # iterate through the array of the digits of guess and secret and compare the digits
+    # i is index for guess; j is index for secret
     for i in range(digits):
         for j in range(digits):
-            # iterate through the array of the digits of guess and secret and compare the digits
-            if nth_digit(guess, i) == nth_digit(secret, j):
-                if nth_digit(secret, j) in SecretArray:
-                    # correctPosition
-                    if i == j:
-                        correctPosition += 1
-                    # wrongPosition
-                    else:
-                        wrongPosition += 1
-                        
+            if nth_digit(guess, i) == nth_digit(secret, j) and nth_digit(secret, j) in SecretArray:
+                # correctPosition
+                if i == j:
+                    correctPosition += 1
                     SecretArray[j] = -1
 
-    
+    for i in range(digits):
+        for j in range(digits):
+            if nth_digit(guess, i) == nth_digit(secret, j) and nth_digit(secret, j) in SecretArray:
+                if i != j:
+                    wrongPosition += 1
+                    SecretArray[j] = -1
+                break
+
     # Problem: how to remember w duplicate numbers
     # (secret = 123; guess = 242; wrongPosition == 1)
     
-    print("wrong position:", wrongPosition, "correct position:", correctPosition)
+    print("wrong position:", wrongPosition, "correct position:", correctPosition, "\n")
     # print(secret)
     
     if guess > 1000:
