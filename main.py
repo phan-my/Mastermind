16dd1fe1134cb9f2b55a51112509485b2443fe5f
Phan MH <phan.manh0hung@gmail.com>
2024-05-13 13:17:09 +0200

    holy shit it works

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 from random import randint
 
+def nth_digit(x, nthDigit):
+    return x//10**nthDigit % 10
+
 digits = 3
 health = 10
 secret = randint(0, 10**(digits) - 1)
+SecretArray = [0] * digits
+
+
 guess = -1
 
 while secret != guess: # NICHT while True
     guess = int(input("Guess the number: "))
     correctPosition = 0
+    wrongPosition = 0
+    chosenNumbers = [0] * 10
     
-    # iteration and comparison
-    # BUG for secret = 189, we have 999 that does not register
-    for i in range(0, digits):
-        if (secret//(10**i)) % 10 == (guess//(10**i)) % 10:
-            correctPosition += 1
+    for i in range(digits):
+        SecretArray[i] = nth_digit(secret, i)
+    
+    for i in range(digits):
+        for j in range(digits):
+            # iterate through the array of the digits of guess and secret and compare the digits
+            if nth_digit(guess, i) == nth_digit(secret, j):
+                if nth_digit(secret, j) in SecretArray:
+                    # correctPosition
+                    if i == j:
+                        correctPosition += 1
+                    # wrongPosition
+                    else:
+                        wrongPosition += 1
+                        
+                    SecretArray[j] = -1
+
     
     # Problem: how to remember w duplicate numbers
-    # (secret = 123; guess = 242; correctNumber == 1)
+    # (secret = 123; guess = 242; wrongPosition == 1)
     
-    print(correctPosition)
+    print("wrong position:", wrongPosition, "correct position:", correctPosition)
     # print(secret)
+    
+    if guess > 1000:
+        print(secret)
+        exit()
     
 print("Correct!")
