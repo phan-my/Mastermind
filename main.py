71b74ea4ede018816ddb5792b34c35c9192f0db9
Phan MH <phan.manh0hung@gmail.com>
2024-05-16 14:51:59 +0200

    Simplify first iteration and add more instances of bug

--- main.py
+++ main.py
@@ -23,25 +23,27 @@ while secret != guess: # NICHT while True
         guess == 111 -> wrongPosition == 2, correctPosition == 1
         guess == 181 -> wrongPosition == 3, correctPosition == 0
         guess == 118 -> "Correct!", wrongPosition == 1, correctPosition == 2
+    secret == 454:
+        guess == 444 -> wrongPosition == 1, correctPosition == 1
+        guess == 454 -> wrongPosition == 1, correctPosition == 1
+    secret == 695:
+        guess = 695 -> wrongPosition == 0, correctPosition == 1
     """
     
-    # iterate through the digits of guess and secret, and compare
-    # i is index for guess; j is index for secret
     # checking correctPosition
+    # if position match, correctPosition++ and rm that _number_
+    #from SecretArray
     for i in range(digits):
-        for j in range(digits):
-            if nth_digit(guess, i) == nth_digit(secret, j) and \
-            nth_digit(guess, i) in SecretArray:
-                # if position match, correctPosition++ and rm that _number_
-                #from SecretArray
-                if i == j:
-                    correctPosition += 1
-                    SecretArray[j] = -1
-                break
+        if nth_digit(guess, i) == nth_digit(secret, i) and \
+        nth_digit(guess, i) in SecretArray:
+            correctPosition += 1
+            SecretArray[i] = -1
+            break
     
-    # iterate 2nd time because wrongPosition has lower priority (to avoid
-    #collisions with correctPostion above); note the i != j
     # checking wrongPosition
+    # iterate 2nd time because wrongPosition has lower priority (to avoid
+    #collisions with correctPostion above); note the i != j , where i is index
+    #for guess and j is index for secret
     for i in range(digits):
         for j in range(digits):
             if nth_digit(guess, i) == nth_digit(secret, j) and \
@@ -49,7 +51,7 @@ while secret != guess: # NICHT while True
                 if i != j:
                     wrongPosition += 1
                     SecretArray[j] = -1
-                break
+                    break
    
     print (
         "wrong position:", wrongPosition,
